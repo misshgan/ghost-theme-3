9481fd014cea8e26ef2558ff066ae1cab6bc2a92
misshgan <medvedev3678@gmail.com>
2022-11-03 12:31:59 +0000

    featured post horizontal section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,14 @@
 //     scrollContainer.scrollLeft += evt.deltaY;
 // });
 
+window.addEventListener('load', function(){
+	let headerHeight = document.querySelector('header').offsetHeight;
+	let hzPanel = document.querySelectorAll('.hz-frame');
+	hzPanel.forEach(element => {
+		element.style.paddingTop = headerHeight + 'px'
+	});
+})
+
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray('.hz-frame');

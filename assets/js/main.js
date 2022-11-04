f781a6bf62de59dc9a54cef18e1f28122bab4f05
misshgan <medvedev3678@gmail.com>
2022-11-04 12:32:49 +0000

    posts feed section done + footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,38 @@
 //     evt.preventDefault();
 //     scrollContainer.scrollLeft += evt.deltaY;
 // });
-
-window.addEventListener('load', function(){
-	let headerHeight = document.querySelector('header').offsetHeight;
-	let hzPanel = document.querySelectorAll('.hz-frame');
-	hzPanel.forEach(element => {
-		element.style.paddingTop = headerHeight + 'px'
-	});
-})
-
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray('.hz-frame');
+if (window.location.pathname = '/') {
+	window.addEventListener('load', function(){
+		let headerHeight = document.querySelector('header').offsetHeight;
+		let hzPanel = document.querySelectorAll('.hz-frame');
+		hzPanel.forEach(element => {
+			element.style.paddingTop = headerHeight + 'px'
+		});
 
-gsap.to(sections, {
-	xPercent: -100 * (sections.length - 1),
-	ease: 'none',
-	scrollTrigger: {
-		trigger: '.hz-wrapper',
-		pin: true,
-		scrub: 1,
-		snap: 1 / (sections.length - 1),
-		end: () => "+=" + document.querySelector('.hz-wrapper').offsetWidth
-	}
-})
+		let header = document.querySelector('.header');
+		header.style.position = `
+			position: fixed;
+			top: 0;
+			left: 0;
+		`
+		let sections = gsap.utils.toArray('.hz-frame');
+	
+		gsap.to(sections, {
+			xPercent: -100 * (sections.length - 1),
+			ease: 'none',
+			scrollTrigger: {
+				trigger: '.hz-wrapper',
+				pin: true,
+				scrub: 1,
+				snap: 1 / (sections.length - 1),
+				end: () => "+=" + document.querySelector('.hz-wrapper').offsetWidth
+			}
+		})
+	})
+}
+
+
+
 // //# sourceMappingURL=main.js.map
